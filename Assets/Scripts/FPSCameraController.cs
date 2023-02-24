7f53e001568dc1f257cffeb8ee38811d0dc4705a
aslantecklead <97795768+aslantecklead@users.noreply.github.com>
2023-02-24 15:03:10 +0300

    initialization commit

---
+++ Assets/Scripts/FPSCameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBoby; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        playerBoby.Rotate(Vector3.up * mouseX);
+    }
+}
